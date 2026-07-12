d559b458cb858eeb99d0ea20302622e2306fde73
Gabriel Broussard Korr <mantis9877@gmail.com>
2026-07-12 16:09:53 -0700

    efficient descendant algo

--- R/analysis.R
+++ R/analysis.R
@@ -7,5 +7,44 @@
 
 
 
+Network = function(links){
+	links = links[links[,1] > 0,] #only existing links. still indexed by [lid]
+
+	network = cbind(
+		links[,3], # X
+		links[,4], # Y
+
+		links[,7], #parent lid (lid = row index in links[]/network[])
+		links[,8], #child 1 lid
+		links[,9], #child 2 lid
+
+		TRUE, #has this link not been counted yet for descendants?
+
+		links[,6], #generation
+		0, # strahler
+		0, # leaves
+		0  # descendants
+	)
+
+
+
+	#calculate leaves
+	leaf_lids = which(network[,4] + network[,5] == 0) #childless nodes
+	for (lid in leaf_lids){ #for each leaf, follow parents to progenitor
+		node = lid
+		d = 0
+		while(network[node,3] != 0){ #the initial link has a parent of "0"
+			#parents are strictly older than children, so this cannot hang
+			d = d + network[node,6]
+			p = network[node,3] #parent lid
+			network[p,8] = network[p,8] + 1 #add leaf count
+			network[p,9] = network[p,9] + d #add descendants count
+			network[node,6] = FALSE #mark node as counted for descendants
+			node = p
+		}
+	}
+
+	network
+}
 
 
--- R/visualization.R
+++ R/visualization.R
@@ -1,20 +1,20 @@
 
 
 
-draw = function(sim,type,fun=NULL){
+draw = function(sim,type){
 	region = c(0,sim$state$p_rules$size)
 	plot(NULL,xlab='',ylab='',axes=FALSE,xlim = region, ylim = region)
 
 	state = sim$state
 	switch(type,
-		l=,links = draw_links(state,fun), #wow I can't believe switch works like this
-		t=,tris = draw_tris(state,fun),
-		n=,network = draw_network(state,fun),
+		l=,links = draw_links(state), #wow I can't believe switch works like this
+		t=,tris = draw_tris(state),
+		n=,network = draw_network(state),
 		warning("Unknown draw type. Options: 'links', 'tris', 'network'") #"l", "t", "n" also work
 	)
 }
 
-draw_links = function(state,fun=NULL){
+draw_links = function(state){
 	particles=state$particles
 	links=state$links
 	for (l in 1:sum(links[,1] != 0)){
@@ -24,8 +24,7 @@ draw_links = function(state,fun=NULL){
 	}
 }
 
-draw_tris = function(state,col=NULL){
-	if (is.null(col)) col = function(link, state) "black"
+draw_tris = function(state){
 	particles=state$particles
 	links=state$links
 	for (l in 1:sum(links[,1] != 0)){
@@ -35,25 +34,18 @@ draw_tris = function(state,col=NULL){
 		pid3 = links[link[7],2] #get the parent's endpoint that's not included in this link
 		#this double-draws most links, which isn't great.
 
-		polygon(particles[c(pid1,pid2,pid3),1:2],border=NA,col=col(link,state))
+		polygon(particles[c(pid1,pid2,pid3),1:2],border=NA,col='black')
 	}
 }
 
-draw_network = function(state,lwd=NULL){
-	if (is.null(lwd)) lwd = function(link, state) 1
+draw_network = function(state){
 	links=state$links
 	for (l in 1:sum(links[,1] != 0)){
 		link = links[l,]
 		parent_link = links[link[7],]
 
-		lines(rbind(link[3:4],parent_link[3:4]),lwd=lwd(link,state))
+		lines(rbind(link[3:4],parent_link[3:4]))
 	}
 }
-
-
-
-#example: draw tris colored by generation
-#draw(sim,'t',\(link,state)rainbow(max(state$links[,6]))[link[6]])
-
 
 
